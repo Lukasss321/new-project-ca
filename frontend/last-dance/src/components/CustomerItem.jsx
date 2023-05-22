74e3a5121391a6616617dd5e886cb6897b713ea4
Lukas Kliucininkas <lukas.kliuc@gmail.com>
2023-05-22 23:40:29 +0300

    AddingClientsTable

--- frontend/last-dance/src/components/CustomerItem.jsx
+++ frontend/last-dance/src/components/CustomerItem.jsx
@@ -1,3 +1,15 @@
+import { Card, Button } from "react-bootstrap";
+
 export const CustomerItem = () => {
-  return <div>CustomerItem</div>;
+  return (
+    <Card className="text-center">
+      <Card.Header>New-Client-Card</Card.Header>
+      <Card.Body>
+        <Card.Title>Please Welcome our newest client</Card.Title>
+        <Card.Text>Welcome!</Card.Text>
+        <Button variant="primary">Add new customer</Button>
+      </Card.Body>
+      {/* <Card.Footer className="text-muted"></Card.Footer> */}
+    </Card>
+  );
 };
