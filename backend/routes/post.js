1d78a612a18ca509969ddd133668fda1a798da93
Lukas Kliucininkas <lukas.kliuc@gmail.com>
2023-05-24 00:05:00 +0300

    SettingUpDB

--- backend/routes/post.js
+++ backend/routes/post.js
@@ -1,10 +1,20 @@
 const express = require("express");
-const { dbConection } = require("../db");
+const { dbConnection } = require("../db");
 const { defaultCallback } = require("../helpers/dbHelper");
 const router = express.Router();
 
 router.get("/post", (req, res) => {
   dbConnection.execute(`SELECT * FROM post`, (err, result) => {
+    defaultCallback(err, result, res);
+  });
+});
+
+router.post("/post", (req, res) => {
+  const { title, text, image } = req.body;
+
+  const postQuery = "INSERT INTO post (title, text, image) VALUES (?, ?, ?)";
+
+  dbConnection.execute(postQuery, [title, text, image], (err, result) => {
     defaultCallback(err, result, res);
   });
 });
